42c11dbfd54dcb2107e5ceac04ca50e51aad4e97
EXT02D36761 <egor.tsedrik@consultant.turkcell.com.tr>
2021-07-29 12:07:22 +0300

    separate components on shared folder

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartComponent} from './chart/chart.component';
-import { ResponsiveDesignCardComponent} from './responsive-design-card/responsive-design-card.component';
-import { WaterWaveTextComponent } from './water-wave-text/water-wave-text.component';
+import { ResponsiveDesignCardComponent} from './cards/responsive-design-card/responsive-design-card.component';
+import { WaterWaveTextComponent } from './text-effects/water-wave-text/water-wave-text.component';
+import { VerticalOpeningTooltipComponent } from './tooltips/vertical-opening-tooltip/vertical-opening-tooltip.component';
 
 
 @NgModule({
   declarations: [
     ChartComponent,
     ResponsiveDesignCardComponent,
-    WaterWaveTextComponent
+    WaterWaveTextComponent,
+    VerticalOpeningTooltipComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/shared/tooltips/vertical-opening-tooltip/vertical-opening-tooltip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-opening-tooltip',
+  templateUrl: './vertical-opening-tooltip.component.html',
+  styleUrls: ['./vertical-opening-tooltip.component.css']
+})
+export class VerticalOpeningTooltipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
