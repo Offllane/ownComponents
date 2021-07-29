437a26777c6f1f5de625586d01b8c4589cadba58
EXT02D36761 <egor.tsedrik@consultant.turkcell.com.tr>
2021-07-29 08:53:43 +0300

    move components to row, add reverse animation for water component

--- src/app/shared/water-wave-text/water-wave-text.component.ts
+++ src/app/shared/water-wave-text/water-wave-text.component.ts
@@ -7,6 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class WaterWaveTextComponent implements OnInit {
   @Input() text: string;
+  @Input() reverse = false;
 
   constructor() { }
 
