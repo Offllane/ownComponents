526ab085dc7a2e935d191bcdb0aad67c1d563765
EXT02D36761 <egor.tsedrik@consultant.turkcell.com.tr>
2021-08-01 22:47:24 +0300

    create: vertical-opening-tooltip

--- src/app/shared/tooltips/vertical-opening-tooltip/vertical-opening-tooltip.component.ts
+++ src/app/shared/tooltips/vertical-opening-tooltip/vertical-opening-tooltip.component.ts
@@ -3,13 +3,70 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-vertical-opening-tooltip',
   templateUrl: './vertical-opening-tooltip.component.html',
-  styleUrls: ['./vertical-opening-tooltip.component.css']
+  styleUrls: ['./vertical-opening-tooltip.component.scss']
 })
 export class VerticalOpeningTooltipComponent implements OnInit {
+  public toggle = false;
+  public menuItems = [
+    {
+      title: 'Cards',
+      icon: 'fa fa-id-card-o',
+      href: '#'
+    },
+    {
+      title: 'Tooltips',
+      icon: 'fa fa-bars',
+      href: '#'
+    },
+    {
+      title: 'Text effects',
+      icon: 'fa fa-font',
+      href: '#'
+    },
+    {
+      title: 'Coming soon',
+      icon: 'fa fa-clock-o',
+      href: '#'
+    },
+    {
+      title: 'Coming soon',
+      icon: 'fa fa-clock-o',
+      href: '#'
+    },
+    {
+      title: 'Coming soon',
+      icon: 'fa fa-clock-o',
+      href: '#'
+    },
+    {
+      title: 'Coming soon',
+      icon: 'fa fa-clock-o',
+      href: '#'
+    },
+    {
+      title: 'Coming soon',
+      icon: 'fa fa-clock-o',
+      href: '#'
+    },
+    {
+      title: 'Coming soon',
+      icon: 'fa fa-clock-o',
+      href: '#'
+    },
+    {
+      title: 'Coming soon',
+      icon: 'fa fa-clock-o',
+      href: '#'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleFunction(): void {
+    this.toggle = !this.toggle;
+
+  }
 }
