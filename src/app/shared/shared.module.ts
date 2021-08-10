1533969bf5241d66078eb734f3cb77b23468461d
EXT02D36761 <egor.tsedrik@consultant.turkcell.com.tr>
2021-08-10 08:51:00 +0300

    move icons to icon foldes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { ResponsiveDesignCardComponent} from './cards/responsive-design-card/res
 import { WaterWaveTextComponent } from './text-effects/water-wave-text/water-wave-text.component';
 import { VerticalOpeningTooltipComponent } from './tooltips/vertical-opening-tooltip/vertical-opening-tooltip.component';
 import { CardWithPlaceholderComponent } from './cards/card-with-placeholder/card-with-placeholder.component';
-import { IsometricIconComponent } from './isometric-icon/isometric-icon.component';
+import { IsometricIconComponent } from './icons/isometric-icon/isometric-icon.component';
 
 
 @NgModule({
